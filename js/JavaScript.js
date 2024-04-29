386f4d5017a138182da68a2f54d683d5759cf44f
Snowflakeeee <110786657+Snowflakeeee@users.noreply.github.com>
2024-04-29 21:48:31 +0300

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,11 @@
+<div id="form_198475_1"></div>
+<script type="text/javascript">
+   (function (d, w, c) {
+       (w[c] = w[c] || []).push({formId:198475,host:"formdesigner.com.ua",formHeight:100, el: "form_198475_1",center: 1,scroll: 0});
+       var s = d.createElement("script"), g = "getElementsByTagName";
+       s.type = "text/javascript"; s.charset="UTF-8"; s.async = true;
+       s.src = (d.location.protocol == "https:" ? "https:" : "http:")+"//formdesigner.com.ua/js/iform.js?v=0.0.3";
+       var h=d[g]("head")[0] || d[g]("body")[0];
+       h.appendChild(s);
+   })(document, window, "fdforms");
+</script>
